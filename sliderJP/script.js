272f9c0c566234e8ed473425aa6770ff79be4e25
João Pedro <joao.pcanhete@gmail.com>
2023-09-08 07:39:41 +0000

    SLIDER

---
+++ sliderJP/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ //so funciona se o site estiver carregado
+    $('.sliderimage ul').slick({
+        autoplay: true, 
+        autoplaySpeed: 2000, // tempo em milisegundos
+        dots: true, // pontinhos
+    });
+});
